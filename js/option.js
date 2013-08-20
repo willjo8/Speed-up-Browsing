0e501b3a38b4e7603fdd29caab1730d6a2fb4de9
mubaidr <mubaidr@gmail.com>
2013-08-20 20:49:11 +0500

    v1.42
    -small changes
    -option page design update

--- js/option.js
+++ js/option.js
@@ -1,25 +1,4 @@
-function save() {
-	var txt_cache = document.getElementById("txt_cache"), st = document.getElementById("txt_status");
-	chrome.extension.getBackgroundPage().txt_cache = localStorage.txt_cache = txt_cache.value;
-	st.innerHTML = "Settings saved...";
-	window.setTimeout(function () {
-		st.innerHTML = "";
-	}, 1500);
-}
-function exit() {
-	chrome.tabs.getCurrent(function (tab) {
-		chrome.tabs.remove(tab.id, function () { });
-	});
-}
-function restore_options() {
-	document.getElementById("txt_cache").value = localStorage.txt_cache;
-}
-window.onload = function () {
-	restore_options();
-	document.getElementById("btn_save").onclick = function () {
-		save();
-	};
-	document.getElementById("btn_exit").onclick = function () {
-		exit();
-	};
-};
+function save(){var txt_cache=document.getElementById("txt_cache"),st=document.getElementById("txt_status");chrome.extension.getBackgroundPage().txt_cache=localStorage.txt_cache=txt_cache.value;st.innerHTML="Settings saved...";window.setTimeout(function(){st.innerHTML="";},1500);}
+function exit(){chrome.tabs.getCurrent(function(tab){chrome.tabs.remove(tab.id,function(){});});}
+function restore_options(){document.getElementById("txt_cache").value=localStorage.txt_cache;}
+window.onload=function(){restore_options();document.getElementById("btn_save").onclick=function(){save();};document.getElementById("btn_exit").onclick=function(){exit();};};
