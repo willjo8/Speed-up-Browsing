ca0d3ca49308ef97bcef39786f4ac0670c07b4be
Muhammad Ubaid Raza <mubaidr@gmail.com>
2013-08-27 23:13:16 +0500

    Storage API

--- js/background.js
+++ js/background.js
@@ -1,10 +1,9 @@
-var txt_cache = '604800';
 chrome.webRequest.onHeadersReceived.addListener(function (object) {
 	'use strict';
 	if (object) {
 		var object_type = object.type.toLowerCase();
 		if ((object_type !== 'main_frame') && (object_type !== 'sub_frame') && (object_type !== 'xmlhttprequest')) {
-			var headers = object.responseHeaders, len = headers.length - 1,	f = false, elem = null;
+			var headers = object.responseHeaders, len = headers.length - 1, f = false, elem = null;
 			do {
 				elem = headers[len];
 				switch (elem.name.toLowerCase()) {
@@ -21,7 +20,7 @@ chrome.webRequest.onHeadersReceived.addListener(function (object) {
 					break;
 				default:
 					break;
-				}			
+				}
 			} while (len--);
 			if (!f) {
 				var obj = {
@@ -38,13 +37,12 @@ chrome.webRequest.onHeadersReceived.addListener(function (object) {
 }, {
 	urls : ['<all_urls>']
 }, ['blocking', 'responseHeaders']);
+var txt_cache = '604800';
 chrome.runtime.onInstalled.addListener(function () {
-	localStorage.run = true;
-	txt_cache = localStorage.txt_cache = '604800';
+	chrome.storage.local.set({
+		'txt_cache' : '604800'
+	});
 });
-if (localStorage.run) {
-	txt_cache = localStorage.txt_cache;
-} else {
-	localStorage.run = true;
-	txt_cache = localStorage.txt_cache = '604800';
-}
+chrome.storage.local.get(function (object) {
+	txt_cache = object['txt_cache'];
+});
