51c799e0f4a79babfb0a3ff1754825e76e74d832
Muhammad Ubaid Raza <mubaidr@gmail.com>
2014-07-26 01:23:09 -0700

    Update headers for only HTTP request
    Do not remove other flags (E-tags, last-modified etc)

--- js/background.js
+++ js/background.js
@@ -1,51 +1,37 @@
 chrome.webRequest.onHeadersReceived.addListener(function (object) {
-	'use strict';
-	if (object) {
-		var object_type = object.type.toLowerCase();
-		if ((object_type !== 'main_frame') && (object_type !== 'sub_frame') && (object_type !== 'xmlhttprequest')) {
-			var headers = object.responseHeaders,
-			len = headers.length - 1,
-			f = false,
-			elem = null;
-			do {
-				elem = headers[len];
-				switch (elem.name.toLowerCase()) {
-				case 'cache-control':
-					if (!f)
-						f = true;
-					break;
-				case 'expires':
-				case 'last-modified':
-				case 'etag':
-					headers.splice(len, 1);
-					break;
-				default:
-					break;
-				}
-			} while (len--);
-			if (!f) {
-				var obj = {
-					'name' : 'Cache-Control',
-					'value' : 'private, max-age=' + txt_cache
-				};
-				headers.push(obj);
-			}
-			return {
-				responseHeaders : headers
-			};
-		}
-	}
+    'use strict';
+    if (object) {
+        var object_type = object.type.toLowerCase();
+        if ((object_type !== 'main_frame') && (object_type !== 'sub_frame') && (object_type !== 'xmlhttprequest')) {
+            var headers = object.responseHeaders,
+                len = headers.length - 1,
+                f = false,
+                elem = null;
+            do {
+                elem = headers[len].name.toLowerCase();
+                if (elem === 'cache-control') {
+                    headers.push({
+                        'name': 'Cache-Control',
+                        'value': 'private, max-age=' + txt_cache
+                    });
+                    return {
+                        responseHeaders: headers
+                    };
+                }
+            } while (!f && len--);
+        }
+    }
 }, {
-	urls : ['<all_urls>']
+    urls: ['http://*/*']
 }, ['blocking', 'responseHeaders']);
 var txt_cache = '604800';
 chrome.runtime.onInstalled.addListener(function () {
-	chrome.storage.local.set({
-		'txt_cache' : '604800'
-	});
+    chrome.storage.local.set({
+        'txt_cache': '604800'
+    });
 });
 chrome.storage.local.get(function (object) {
-	txt_cache = object['txt_cache'];
+    txt_cache = object['txt_cache'];
 });
 /*
 function test(headers, object_type) {
