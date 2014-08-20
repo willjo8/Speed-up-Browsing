b8ea446f060ff2b8d2269bf1705b4e60136c0c28
Muhammad Ubaid Raza <mubaidr@gmail.com>
2014-08-20 20:20:34 +0500

    Enable for all urls and types
    -Remove all old flags related to cache and then add new one
    -Enable for all urls and types

--- js/background.js
+++ js/background.js
@@ -1,53 +1,38 @@
-/*More aggressive settings, working fine*/
-chrome.webRequest.onHeadersReceived.addListener(function (object) {
-	'use strict';
-	if (object) {
-		var object_type = object.type.toLowerCase();
-		if ((object_type !== 'main_frame') && (object_type !== 'sub_frame') && (object_type !== 'xmlhttprequest')) {
-			var headers = object.responseHeaders,
-			len = headers.length - 1,
-			f = false,
-			elem = null;
-			do {
-				elem = headers[len];
-				switch (elem.name.toLowerCase()) {
-				case 'cache-control':
-					/*if (!f) {
-						f = true;
-						headers[len].value = 'private, max-age=' + txt_cache;
-					}
-					
-					break;*/
-				case 'expires':
-				case 'last-modified':
-				case 'etag':
-					headers.splice(len, 1);
-					break;
-				default:
-					break;
-				}
-			} while (len--);
-			//if (!f) {
-				var obj = {
-					'name' : 'Cache-Control',
-					'value' : 'private, max-age=' + txt_cache
-				};
-				headers.push(obj);
-			//}
-			return {
-				responseHeaders : headers
-			};
-		}
-	}
-}, {
-	urls : ['<all_urls>']
-}, ['blocking', 'responseHeaders']);
-var txt_cache = '31536000';
-chrome.runtime.onInstalled.addListener(function () {
-	chrome.storage.local.set({
-		'txt_cache' : '31536000'
-	});
-});
-chrome.storage.local.get(function (object) {
-	txt_cache = object['txt_cache'];
-});
+/*global chrome, console*/
+(function () {
+    'use strict';
+    var txt_cache = '31536000';
+    chrome.runtime.onInstalled.addListener(function () {
+        chrome.storage.local.set({
+            'txt_cache': '31536000'
+        });
+    });
+    chrome.storage.local.get(function (object) {
+        txt_cache = object.txt_cache;
+    });
+    chrome.webRequest.onHeadersReceived.addListener(function (object) {
+        if (object) {
+            var object_type = object.type.toLowerCase(),
+                headers = object.responseHeaders,
+                len = headers.length - 1,
+                elemName = null,
+                obj = {
+                    'name': 'cache-control',
+                    'value': 'private, max-age=' + txt_cache
+                };
+            do {
+                elemName = headers[len].name.toLowerCase();
+                if (elemName === 'cache-control' || elemName === 'expires' || elemName === 'last-modified' || elemName === 'etag' || elemName === 'age') {
+                    headers.splice(len, 1);
+                }
+                len -= 1;
+            } while (len !== 0);
+            headers.push(obj);
+            return {
+                responseHeaders: headers
+            };
+        }
+    }, {
+        urls: ['<all_urls>']
+    }, ['blocking', 'responseHeaders']);
+}());
