06a5207024c52d3184fc00a0f83cf5dec7b031af
Muhammad Ubaid Raza <mubaidr@gmail.com>
2014-08-21 08:55:04 +0500

    na

--- js/background.js
+++ js/background.js
@@ -15,18 +15,18 @@
             var object_type = object.type.toLowerCase(),
                 headers = object.responseHeaders,
                 len = headers.length - 1,
-                elemName = null,
+                tag = null,
                 obj = {
                     'name': 'cache-control',
                     'value': 'private, max-age=' + txt_cache
                 };
             do {
-                elemName = headers[len].name.toLowerCase();
-                if (elemName === 'cache-control' || elemName === 'expires' || elemName === 'last-modified' || elemName === 'etag' || elemName === 'age') {
+                tag = headers[len].name.toLowerCase();
+                if (tag === 'cache-control' || tag === 'expires' || tag === 'last-modified' || tag === 'etag' || tag === 'age') {
                     headers.splice(len, 1);
                 }
                 len -= 1;
-            } while (len !== 0);
+            } while (len > 0);
             headers.push(obj);
             return {
                 responseHeaders: headers
